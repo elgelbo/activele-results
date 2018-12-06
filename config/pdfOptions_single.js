efa3287f62c8866a89e5da25184e6021d2053cd0
el_gelbo <mgelbman@gmail.com>
2018-12-06 12:28:38 -0800

    working?

---
+++ config/pdfOptions_single.js
@@ -0,0 +1,29 @@
+const singleResult_PDFoptions = {
+  // Papersize Options: http://phantomjs.org/api/webpage/property/paper-size.html
+  "format": "Letter",        // allowed units: A3, A4, A5, Legal, Letter, Tabloid
+  "orientation": "portrait", // portrait or landscape
+
+  // Page options
+  "border": {
+    "top": "0",            // default is 0, units: mm, cm, in, px
+    "right": "1in",
+    "bottom": "0in",
+    "left": "1in"
+  },
+
+  paginationOffset: 1,       // Override the initial pagination number
+  "header": {
+    "height": "0.75in",
+    "contents": '<div style="text-align: right;color: grey; font-size: 10px; font-family: Roboto;">Individual Responses - ActiveLE Text Message</div>'
+  },
+  "footer": {
+    "height": "0.75in",
+    "contents": {
+      default: '<div style="text-align: right;color: grey; font-size: 10px; font-family: Roboto;">{{page}} of {{pages}}', // fallback value
+    }
+  },
+  // Rendering options
+  "base": "file:///C:/Users/mgelbman/Desktop/node/activeLE-results/dist/", // Base path that's used to load files (images, css, js) when they aren't referenced using a host
+}
+
+module.exports = singleResult_PDFoptions;
--- convert.js
+++ convert.js
@@ -3,7 +3,10 @@ const ejs = require('ejs');
 var pdf = require('html-pdf');
 var axios = require('axios')
 const endPoint = 'http://activele-survey.herokuapp.com/api/results'
-const summary = require('./summary')
+const summary = require('./summary');
+const singleOptions = require('./config/pdfOptions_single')
+const summaryOptions = require('./config/pdfOptions_single')
+const questions = require('./config/survey_eng')
 
 var options = {
   // Papersize Options: http://phantomjs.org/api/webpage/property/paper-size.html
@@ -21,16 +24,16 @@ var options = {
   paginationOffset: 1,       // Override the initial pagination number
   "header": {
     "height": "0.75in",
-    "contents": '<div style="text-align: center;">ActiveLE Text Message Survey Summary</div>'
+    "contents": '<div style="text-align: right;color: grey; font-size: 10px; font-family: Roboto;">Individual Responses - ActiveLE Text Message</div>'
   },
   "footer": {
     "height": "0.75in",
     "contents": {
-      default: '<span>{{page}}</span> of <span>{{pages}}</span>', // fallback value
+      default: '<div style="text-align: right;color: grey; font-size: 10px; font-family: Roboto;">{{page}} of {{pages}}', // fallback value
     }
   },
   // Rendering options
-  "base": "file:///C:/Users/mgelbman/Desktop/node/activeLE-results/src/", // Base path that's used to load files (images, css, js) when they aren't referenced using a host
+  "base": "file:///C:/Users/mgelbman/Desktop/node/activeLE-results/dist/", // Base path that's used to load files (images, css, js) when they aren't referenced using a host
 }
 
 function writeFileStream(filePath, data) {
@@ -45,27 +48,35 @@ function writeFileStream(filePath, data) {
 }
 
 const createPDF = (goods) => {
-  pdf.create(goods, options).toFile('./pdf/results.pdf', function (err, res) {
+  pdf.create(goods, singleOptions).toFile('./pdf/result.pdf', function (err, res) {
     if (err) return console.log(err);
-    console.log(res); // { filename: '/app/businesscard.pdf' }
+    console.log(res);
   });
 }
 
-const ejsMaker = async (money, clean) => {
-  var htmlContent = fs.readFileSync(__dirname + '/views/results.ejs', 'utf8');
-  var htmlRenderized = ejs.render(htmlContent, { filename: 'results.ejs', money: money, clean: clean });
-  writeFileStream('./src/results.html', htmlRenderized);
+const ejsSingle = async (money, questions) => {
+  var htmlContent = fs.readFileSync(__dirname + '/views/result.ejs', 'utf8');
+  var htmlRenderized = ejs.render(htmlContent, { filename: 'result.ejs', surveys: money.surveys, questions: questions });
+  writeFileStream('./dist/results.html', htmlRenderized);
   return htmlRenderized;
 }
 
+const ejsSummary = async (money, clean, questions) => {
+    var htmlContent = fs.readFileSync(__dirname + '/views/results.ejs', 'utf8');
+    var htmlRenderized = ejs.render(htmlContent, { filename: 'results.ejs', surveys: money.surveys, summary: clean, questions: questions });
+    writeFileStream('./dist/results.html', htmlRenderized);
+    return htmlRenderized;
+  }
+
+  
 const go = async () => {
   try {
     const response = await axios.get(endPoint);
     const clean = summary.pipeline(response.data.surveys);
-    // console.log(clean); 
-    const pretty = await ejsMaker(response.data, clean);
-    createPDF(pretty);
-    // console.log(pretty);
+    // const prettySingle = await ejsSingle(response.data, questions);
+    const prettySummary = await ejsSummary(response.data, clean, questions);
+    // createPDF(prettySingle);
+    // createPDF(prettySummary);
   } catch (error) {
     console.error(error);
   }
