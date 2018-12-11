b4d2d5a11eb4336e57f32f8e64a22e4b75cf4270
el_gelbo <mgelbman@gmail.com>
2018-12-11 15:19:40 -0800

    add click for flow lines

--- dist/js/odMap.js
+++ dist/js/odMap.js
@@ -4,7 +4,7 @@ function createODMap(center, myGeo) {
     center[1], center[0]
   ], 9);
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 18,
+    maxZoom: 12,
     id: 'mapbox.streets'
   }).addTo(mymap);    
 
@@ -16,12 +16,12 @@ function createODMap(center, myGeo) {
     animationEasingType: 'In',
     animationDuration: 2000
   }).addTo(mymap);
-  // oneToManyFlowmapLayer.on('click', function(e) {
-  //   if (e.sharedOriginFeatures.length) {
-  //     oneToManyFlowmapLayer.selectFeaturesForPathDisplay(e.sharedOriginFeatures, 'SELECTION_NEW');
-  //   }
-  //   if (e.sharedDestinationFeatures.length) {
-  //     oneToManyFlowmapLayer.selectFeaturesForPathDisplay(e.sharedDestinationFeatures, 'SELECTION_NEW');
-  //   }
-  // });
+  oneToManyFlowmapLayer.on('click', function(e) {
+    if (e.sharedOriginFeatures.length) {
+      oneToManyFlowmapLayer.selectFeaturesForPathDisplay(e.sharedOriginFeatures, 'SELECTION_NEW');
+    }
+    if (e.sharedDestinationFeatures.length) {
+      oneToManyFlowmapLayer.selectFeaturesForPathDisplay(e.sharedDestinationFeatures, 'SELECTION_NEW');
+    }
+  });
 }
